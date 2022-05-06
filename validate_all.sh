353c1639c177e4e1a4778e0e6a1d46125fe11c25
HendrikGematik <hendrik.jablonski@gematik.de>
2022-05-06 10:03:12 +0200

    fixed batch validation script

--- validate_all.sh
+++ validate_all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
  echo -e "Starting Sushi";
- sushi Resources
+# sushi Resources
  for filename in $(find ./Resources -name '*.json');
  do
-    echo -e "Processing \033[1m $filename \033[0m";
-    java -jar ../validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core -ig "$filename" -proxy 192.168.110.10:3128 -output "val_%a.html";
+    f="$(basename $filename .json)"
+    echo -e "Processing \033[1m $f \033[0m";
+    java -jar ../validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core $filename -proxy 192.168.110.10:3128 -output "../val_out/$f.html";
  done
 
  #for directory in $(find -type d -name "Resources");
  #do
  #   echo -e "Processing \033[1m $directory \033[0m";
-  #  java -jar validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core -ig "$directory" -proxy 192.168.110.10:3128 -output "val_%a.html";
+  #  java -jar validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core -ig "$directory" $f -proxy 192.168.110.10:3128 -output "val_%a.html";
  #done
