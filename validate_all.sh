f39960ec8879709eb20441bcfa6e4a93a780cfb5
SvenSommer <rob.hoffmann@posteo.de>
2022-05-06 15:23:54 +0200

    updated script to load missing resourses

--- validate_all.sh
+++ validate_all.sh
@@ -23,10 +23,10 @@ fhir bake --package de.gematik.erezept-workflow.r4
 echo -e "Starting Sushi";
 sushi Resources
 
- # iterate of files
- for filename in $(find ./Resources -name '*.json');
- do
-    f="$(basename $filename .json)"
-    echo -e "Processing \033[1m $f \033[0m";
-    java -jar $VALIDATOR -version 4.0.1 -ig hl7.fhir.r4.core -ig de.gematik.erezept-workflow.r4#1.2.0-rc1 -ig kbv.ita.erp#1.0.2 -ig de.abda.eRezeptAbgabedatenPKV#1.1.0-rc9 $filename -proxy 192.168.110.10:3128 -output "../val_out/$f.html";
- done
+# iterate of files
+for filename in $(find ./Resources -name '*.json');
+do
+   f="$(basename $filename .json)"
+   echo -e "Processing \033[1m $f \033[0m";
+   java -jar $VALIDATOR -version 4.0.1 -ig hl7.fhir.r4.core -ig de.gematik.erezept-workflow.r4#1.2.0-rc1 -ig kbv.ita.erp#1.0.2 -ig de.abda.eRezeptAbgabedatenPKV#1.1.0-rc9 $filename -proxy 192.168.110.10:3128 -output "../val_out/$f.html";
+done
