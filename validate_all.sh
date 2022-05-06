0ffafc047d4e06cce8ffe6f98f4f5396a1ef5c0f
HendrikGematik <hendrik.jablonski@gematik.de>
2022-05-06 13:41:28 +0200

    packages für validierung ergänzt

--- validate_all.sh
+++ validate_all.sh
@@ -5,7 +5,7 @@
  do
     f="$(basename $filename .json)"
     echo -e "Processing \033[1m $f \033[0m";
-    java -jar ../validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core $filename -proxy 192.168.110.10:3128 -output "../val_out/$f.html";
+    java -jar ../validator_cli.jar -version 4.0.1 -ig hl7.fhir.r4.core -ig de.gematik.erezept-workflow.r4#1.2.0-rc1 -ig kbv.ita.erp#1.0.2 -ig de.abda.eRezeptAbgabedatenPKV#1.1.0-rc9 $filename -proxy 192.168.110.10:3128 -output "../val_out/$f.html";
  done
 
  #for directory in $(find -type d -name "Resources");
