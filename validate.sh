f3d31dc13b04fb57d319559e80be6a88f253aecc
HendrikGematik <hendrik.jablonski@gematik.de>
2022-08-16 16:48:04 +0200

    include local profiles in validator

--- validate.sh
+++ validate.sh
@@ -63,6 +63,7 @@ then
   # load dependencies from sushi-config.yaml
   for dependency in `yq -o=props '.dependencies' $foldername/sushi-config.yaml`;
   do
+    # TODO dont load LATEST -> get by file version
     if [[ ${dependency::1} =~ [a-z] ]]
     then
       # create FHIR Snapshots of dependencies
@@ -72,6 +73,15 @@ then
     fi
   done
 fi
+
+# rename all folders to lower case
+echo -e "Rename all folder name in .fhir folder to lower case";
+for i in `( ls -d $fhir_folder_path/* | grep [A-Z] )`;
+do
+  echo  $i
+  rsync -a $i `echo $i | tr 'A-Z' 'a-z'`
+# mv -i $i `echo $i | tr 'A-Z' 'a-z'`;
+done
 
 # run sushi
 echo -e "Starting Sushi to process files in '$foldername'";
@@ -104,7 +114,9 @@ then
 else
    echo -e "Processing \033[1m $file \033[0m";
    f="$(basename $file .json)"
-   java -jar $validatorpath -version 4.0.1 -ig $foldername/fsh-generated/resources $file -proxy 192.168.110.10:3128 -output $outputfolder"/$f.html";
+   # foreach package in -ig ~/.fhir/packages/*
+   #   += "-ig $package/package"
+   java -jar $validatorpath -version 4.0.1 -ig ~/.fhir/packages/de.basisprofil.r4#1.3.2/package  -ig $foldername/fsh-generated/resources $file -proxy 192.168.110.10:3128 -output $outputfolder"/$f.html";
    if [ $sort_results == "true" ]
     then
       sortBySeverity $outputfolder"/$f.html"
